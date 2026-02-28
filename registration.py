b3e4da62be5c04aaf604cf7a897a5be5f179c8ed
RobertManniste <robert.manniste@gmail.com>
2026-02-28 14:47:59 +0200

    Create registration.py

---
+++ registration.py
@@ -0,0 +1,8 @@
+from telegram import Update
+from telegram.ext import ContextTypes
+
+
+async def start_registration(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "Регистрация скоро будет доступна 👤"
+    )
